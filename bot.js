94188c0597101d1bf0f598721341452619a1e6ab
REKOYT <46628323+REKOYT@users.noreply.github.com>
2019-01-12 18:37:00 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("533565936788045824")
+setInterval(function() {
+channel.send(`REKOYT REKOYT REKOYT REKOYT REKOYT REKOYT`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
